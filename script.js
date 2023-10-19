7614941c26097e320c1ca7d4df9e8ea6cdabd470
Piotr Obara <pobara@o2.pl>
2023-10-19 06:16:04 +0200

    build js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const inputBill = document.querySelector(".main__inputSection__inputBill");
+const inputBillAlert = document.querySelector(".main__inputSection__label__alert");
 
 const btn5percent = document.querySelector(".b1");
 const btn10percent = document.querySelector(".b2");
@@ -16,8 +17,41 @@ const outputPricePerPerson = document.querySelector(".outputSection__totalWrappe
 
 const resetBtn = document.querySelector(".outputSection__reset");
 
-function calculateTipPercent (totalPrice, percent) {
-const result =  (totalPrice * percent) / 100;
-console.log(result);
+
+
+function validateAccount() {
+   
+
+    const accountValue = inputBill.value;
+    const cleanedInput = accountValue.replace(/\s/g, '');
+
+    if (/^\d+$/.test(cleanedInput) && parseInt(cleanedInput, 10) <= 1000000000) {
+        inputBillAlert.textContent = "";
+      
+    } else {
+        inputBillAlert.textContent = "Only digits and max $1000000000";
+    }
 }
 
+inputBill.addEventListener("input", validateAccount);
+
+
+
+
+
+function calculateTipPercent (percent) {
+    
+    const totalPrice = parseFloat(inputBill.value);
+    if(!isNaN(totalPrice)) {
+    const result =  (totalPrice * percent) / 100;
+    console.log(result);}
+    else {
+        console.log("podaj liczbę a nie litere")
+    }
+    }
+
+btn5percent.addEventListener("click", calculateTipPercent(5));
+btn10percent.addEventListener("click", calculateTipPercent(10));
+btn15percent.addEventListener("click", calculateTipPercent(15));
+btn25percent.addEventListener("click", calculateTipPercent(25));
+btn50percent.addEventListener("click", calculateTipPercent(50));
